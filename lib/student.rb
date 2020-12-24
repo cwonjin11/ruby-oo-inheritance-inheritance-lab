172d95d178eec264ee9e5f2abcebaa0b18bae0fc
Wonjin <cwonjin1@gmail.com>
2020-12-24 15:53:01 -0500

    commit tree 8697a91d18d98c5807a820a6baaff87445c9d7e2

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(stirng)
+       @knowledge << stirng 
+    end
+ 
+    def knowledge
+        @knowledge
+    end
+    
 end
